dcadb5f92815c1fb663804f1ab9518cb7558afa7
Aycan Keser <aycankeser@gmail.com>
2019-01-04 01:25:55 +0300

    cors added

--- NetCode21Mongo/NetCode21Mongo/Startup.cs
+++ NetCode21Mongo/NetCode21Mongo/Startup.cs
@@ -15,6 +15,7 @@ namespace NetCode21Mongo
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
